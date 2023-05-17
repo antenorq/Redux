15717de8fc68216a7ba125a47a225bd3168f5069
antenorq <antenorq@gmail.com>
2023-05-17 00:49:53 -0400

    updated with add quantity state redux

--- src/redux/cart/reducer.js
+++ src/redux/cart/reducer.js
@@ -8,9 +8,27 @@ const initialState = {
 const cartReducer = (state = initialState, action) => {
   switch (action.type) {
     case CartActionTypes.ADD_PRODUCTS:
+      //check if this product is already into the cart
+      const productIsAlreadyInCart = state.products.some(
+        (product) => product.id === action.payload.id
+      );
+
+      //if already have this product in the car add quantity +1
+      if (productIsAlreadyInCart) {
+        return {
+          ...state,
+          products: state.products.map((product) =>
+            product.id === action.payload.id
+              ? { ...product, quantity: product.quantity + 1 }
+              : product
+          ),
+        };
+      }
+
+      //if does not have this product into the car add it
       return {
-        ...initialState,
-        products: [...initialState.products, action.payload],
+        ...state,
+        products: [...state.products, { ...action.payload, quantity: 1 }],
       };
 
     default:
